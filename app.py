479091daaeca55c6484f8c8a513c07e6d1998786
IamSIJ <smahad978@gmail.com>
2025-02-20 11:32:09 +0500

    Added Privacy Policy

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__, static_folder='static')
 def home():
     return render_template("home.html")
 
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
 if __name__ == "__main__":
     app.run('0.0.0.0', port=5000, debug=True)
